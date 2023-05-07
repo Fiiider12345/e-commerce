b87d447ae62448f3fdde0ef90f4c0f14a87d5bcb
Fiiider12345 <marek.pecho0@gmail.com>
2023-05-07 15:03:16 +0200

    Created API to Upload Images

--- src/main/java/com/ecommerce/entity/Product.java
+++ src/main/java/com/ecommerce/entity/Product.java
@@ -1,9 +1,16 @@
 package com.ecommerce.entity;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Product {
@@ -16,6 +23,25 @@ public class Product {
 	private Double productDiscountedPrice;
 	private Double productActualPrice;
 	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(name = "product_images",
+		joinColumns = {
+				@JoinColumn(name = "product_id")
+		},
+		inverseJoinColumns = {
+				@JoinColumn(name = "image_id")
+		}
+	)
+	private Set<ImageModel> productImages;
+	
+	
+	
+	public Set<ImageModel> getProductImages() {
+		return productImages;
+	}
+	public void setProductImages(Set<ImageModel> productImages) {
+		this.productImages = productImages;
+	}
 	public Integer getProductId() {
 		return productId;
 	}
